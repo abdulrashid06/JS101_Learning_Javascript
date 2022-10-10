61429b850579ee42a0e0937ceb060fed8aef1ef2
abdulrashid06 <abdulrashid987655@gmail.com>
2022-10-10 19:57:18 +0530

    program

---
+++ Day_2_programs/problem3.js
@@ -0,0 +1,13 @@
+let a=10;
+let b=11;
+
+if (a>b){
+  console.log(" a is greater");
+}else if (b>a){
+  console.log("b is greater");
+}else{
+  console.log("both equal");
+}
+
+ 
+
---
+++ Day_2_programs/problem4.js
@@ -0,0 +1,18 @@
+let database_username="raj@gmail.com";
+let database_password="raj123";
+
+//login
+
+let username="raj@gmail.com";
+let password="raj123";
+
+if (database_username==username){
+  if (database_password==password){
+
+    console.log("Login Successful");
+  }else{
+    console.log("Incorrect Password");
+  }
+}else{
+  console.log("Wrong Credentials");
+}
